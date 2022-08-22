97eb3effe99186eef9734e1c3360811ddb363d65
Blackey <79453075+BlackTitle@users.noreply.github.com>
2022-08-22 16:30:13 +0800

    改用for in循环判断
    👴不用if无限套了

--- E7score.py
+++ E7score.py
@@ -1,174 +1,115 @@
+import random
 import hoshino
 from hoshino import Service, priv
 from hoshino.typing import CQEvent
 
-sv = Service('E7分数', manage_priv=priv.SUPERUSER, help_='请输入:分数 属性名 属性数值 属性名 属性数值 属性名 属性数值 属性名 属性数值\n例:分数 爆伤 1 暴击 2 生命 3 攻击 4')
+
+sv = Service('E7分数', manage_priv=priv.SUPERUSER, help_='请输入：分数 名1 值1 名2 值2 名3 值3 名4 值4\n如：分数 攻击 1 攻击 2 攻击 3 攻击 4\n')
 
 @sv.on_prefix('分数')
 async def feedback(bot, ev: CQEvent):
-    code = ev.raw_message
-    code = code.replace('%','')
-    qiege = code.split()
-    tx = 0
-    shuchu=0
+    vaule = ev.raw_message
+    ge = vaule.split()
+    #不知道为什么总是出错误，搞个字典设置禁止
+    ciku = ['生命','防御','攻击','暴击','爆伤','速度','命中','抗性','分数']
+    #各类总分
+    atk = 0
+    rou = 0
+    ob = 0
+    score = 0
 
-    if len(qiege)>9 or len(qiege)%2 == 0:
-        tixing="您的指令输入的有问题,请检查后输入\n格式为:分数 爆伤 1 暴击 2 生命 3 攻击 4"
-        await bot.send(ev, tixing)
+    #适用人群 这一块不知道该咋写了 等有机会再写吧XD
+    #大概的思路就是结合某几种的分 去计算适合哪个角色   比如生命+暴击+爆伤>x 那就是适合暗萝 就输出banrou=1
+    banrou = 0 #半肉 暗萝等
+    huotui = 0 #特指火腿
+    tongc = 0 #普通c 比如暗法？
+    tongob = 0 #普通辅助 比如光ll
+    tongrou = 0 #普通肉 拉斯 暗盾等
+
+    #整几句骚话
+    laji = ['你这装备垃圾桶捡来的？', '这么点分...你尽力了', '在某种层面上讲,兄弟你很欧洲'] #总分<50
+    yiban  = ['中规中矩,还算不错', '一人之下,万人之上的利器', '不是KFC疯狂星期四出来的鸡腿一般的普通'] #70 > 总分 >50
+    niubi = ['尚方宝剑,先斩后奏。男中龙，女中风。', '传说是由女娲石打造而成的利器...', '牛逼']
+    wudi = ['无敌'] #>70
+
+    #杂项
+    jiancha = 0
+
+
+    for i in range(0, len(ge)):
+        if len(ge[i]) == 2 and i % 2 != 0:
+            jiancha += 1
+    #检查词条数量
+    if len(ge) % 2 == 0:
+        cuowu = '指令格式有误.\n指令例:分数 攻击 1 攻击 2 攻击 3 攻击 4'
+        await bot.send(ev, cuowu)
         return
-
-#无限套娃的开始
-    if len(qiege) == 9:
-        score1 = float(qiege[2]) #第一个数据
-        score2 = float(qiege[4]) #第二个数据
-        score3 = float(qiege[6]) #第三个数据
-        score4 = float(qiege[8]) #第四个数据
-        name1 = str(qiege[1])  # 第一个数据名
-        name2 = str(qiege[3])  # 第二个数据名
-        name3 = str(qiege[5])  # 第三个数据名
-        name4 = str(qiege[7])  # 第四个数据名
-        #判断数据名格式
-        if len(name1) != 2 or len(name2) != 2 or len(name3) != 2 or len(name4) != 2:
-            tx = 1
-            return
-#寻找暴击
-        if name1 == "暴击":
-            score1 = score1 * 1.5
-        elif name2 == "暴击":
-            score2 = score2 * 1.5
-        elif name3 == "暴击":
-            score3 = score3 * 1.5
-        elif name4 == "暴击":
-            score4 = score4 * 1.5
-
-#寻找爆伤
-        if name1 == "爆伤":
-            score1 = score1 * 1.125
-        elif name2 == "爆伤":
-            score2 = score2 * 1.125
-        elif name3 == "爆伤":
-            score3 = score3 * 1.125
-        elif name4 == "爆伤":
-            score4 = score4 * 1.125
-
-#寻找速度
-        if name1 == "速度":
-            score1 = score1 * 2
-        elif name2 == "速度":
-            score2 = score2 * 2
-        elif name3 == "速度":
-            score3 = score3 * 2
-        elif name4 == "速度":
-            score4 = score4 * 2
-
-        scorehe = score1 + score2 + score3 + score4
-        shuchu = f'您这件装备的分数为:\n{name1}:{score1}分\n{name2}:{score2}分\n{name3}:{score3}分\n{name4}:{score4}分\n总分为 {scorehe}分.'
-
-#无限套娃2
-    if len(qiege) == 7:
-        score1 = float(qiege[2]) #第一个数据
-        score2 = float(qiege[4]) #第二个数据
-        score3 = float(qiege[6]) #第三个数据
-        name1 = str(qiege[1])  # 第一个数据名
-        name2 = str(qiege[3])  # 第二个数据名
-        name3 = str(qiege[5])  # 第三个数据名
-
-        #判断数据名格式
-        if len(name1) != 2 or len(name2) != 2 or len(name3) != 2:
-            tx = 1
-            return
-#寻找暴击
-        if name1 == "暴击":
-            score1 = score1 * 1.5
-        elif name2 == "暴击":
-            score2 = score2 * 1.5
-        elif name3 == "暴击":
-            score3 = score3 * 1.5
-
-#寻找爆伤
-        if name1 == "爆伤":
-            score1 = score1 * 1.125
-        elif name2 == "爆伤":
-            score2 = score2 * 1.125
-        elif name3 == "爆伤":
-            score3 = score3 * 1.125
-
-#寻找速度
-        if name1 == "速度":
-            score1 = score1 * 2
-        elif name2 == "速度":
-            score2 = score2 * 2
-        elif name3 == "速度":
-            score3 = score3 * 2
-
-        scorehe = score1 + score2 + score3
-        shuchu = f'您这件装备的分数为:\n{name1}:{score1}分\n{name2}:{score2}分\n{name3}:{score3}分\n总分为 {scorehe}分.'
-
-
-#无限套娃2
-    if len(qiege) == 5:
-        score1 = float(qiege[2]) #第一个数据
-        score2 = float(qiege[4]) #第二个数据
-        name1 = str(qiege[1])  # 第一个数据名
-        name2 = str(qiege[3])  # 第二个数据名
-
-
-        #判断数据名格式
-        if len(name1) != 2 or len(name2) != 2:
-            tx = 1
-            return
-#寻找暴击
-        if name1 == "暴击":
-            score1 = score1 * 1.5
-        elif name2 == "暴击":
-            score2 = score2 * 1.5
-
-#寻找爆伤
-        if name1 == "爆伤":
-            score1 = score1 * 1.125
-        elif name2 == "爆伤":
-            score2 = score2 * 1.125
-
-#寻找速度
-        if name1 == "速度":
-            score1 = score1 * 2
-        elif name2 == "速度":
-            score2 = score2 * 2
-
-        scorehe = score1 + score2
-        shuchu = f'您这件装备的分数为:\n{name1}:{score1}分\n{name2}:{score2}分\n总分为 {scorehe}分.'
-
-
-#无限套娃2
-    if len(qiege) == 3:
-        score1 = float(qiege[2]) #第一个数据
-        name1 = str(qiege[1])  # 第一个数据名
-
-        #判断数据名格式
-        if len(name1) != 2:
-            tx = 1
-            return
-#寻找暴击
-        if name1 == "暴击":
-            score1 = score1 * 1.5
-
-#寻找爆伤
-        if name1 == "爆伤":
-            score1 = score1 * 1.125
-
-#寻找速度
-        if name1 == "速度":
-            score1 = score1 * 2
-
-        scorehe = score1
-        shuchu = f'您这件装备的分数为:\n{name1}:{score1}分\n总分为 {scorehe}分.'
-
-    if tx == 1:
-        tixing = "您输入的数据名称有问题,请检查后重新输入\n提醒:暴击伤害输入爆伤,暴击率输入暴击"
-        await bot.send(ev, tixing)
+    #检查格式防止出现错误
+    elif jiancha == 5:
+        cuowu = '指令格式有误。\n暴击伤害→爆伤、暴击率→暴击、效果命中→命中、效果抗性→抗性\n攻击、防御、生命如是百分比请在前面加. 如:生命 .12'
+        await bot.send(ev, cuowu)
         return
     else:
-        await bot.send(ev, shuchu)
+    #开始循环
+        for i in range(0, len(ge)):
+            #判断是否为百分比
+            if '.' in ge[i]:
+                ge[i] = ge[i].replace('.', '')
 
+            #计算
+            elif '%' not in ge[i] and ge[i] not in ciku:
+                if ge[i-1] == "攻击":
+                    ge[i] = round(float(ge[i]) /  11.27 , 2)
+                elif ge[i-1] == "防御":
+                    ge[i] = round(float(ge[i]) / 6.21 , 2)
+                elif ge[i-1] == "生命":
+                    ge[i] = round(float(ge[i]) / 56.31 , 2)
+
+            #计算百分比特殊数值
+            if ge[i-1] == "暴击":
+                ge[i] = float(ge[i])  * 1.5
+            elif ge[i-1] == "爆伤":
+                ge[i] = float(ge[i])  * 1.125
+            elif ge[i-1] == "速度":
+                ge[i] = float(ge[i])  * 2
+
+            #计算合体分值
+            if ge[i-1] == "攻击" or ge[i-1] == "速度" or ge[i-1] == "暴击" or ge[i-1] == "爆伤" and ge[i] not in ciku :
+                atk = atk + float(ge[i])
+            if ge[i-1] == "生命" or ge[i-1] == "速度" or ge[i-1] == "防御" and ge[i] not in ciku:
+                rou = rou + float(ge[i])
+            if ge[i - 1] == "生命" or ge[i - 1] == "速度" or ge[i - 1] == "命中" or ge[i - 1] == "抗性" and ge[i] not in ciku:
+                ob = ob + float(ge[i])
+
+            #总分
+            if ge[i] not in ciku:
+                score = score + float(ge[i])
+
+        #判断总分
+        if score < 50:
+            talk4 = random.choice(laji)
+        elif score < 70:
+            talk4 = random.choice(yiban)
+        elif score < 80:
+            talk4 = random.choice(niubi)
+        elif score < 999999:
+            talk4 = random.choice(wudi)
+
+
+        #打印
+        talk1 = f'您的这件装备分数为\n'
+        if len(ge) == 9:
+            talk2 = f'{ge[1]}:{ge[2]}分\n{ge[3]}:{ge[4]}分\n{ge[5]}:{ge[6]}分\n{ge[7]}:{ge[8]}分\n'
+        elif len(ge) == 7:
+            talk2 = f'{ge[1]}:{ge[2]}分\n{ge[3]}:{ge[4]}分\n{ge[5]}:{ge[6]}分\n'
+        elif len(ge) == 5:
+            talk2 = f'{ge[1]}:{ge[2]}分\n{ge[3]}:{ge[4]}分\n'
+        elif len(ge) == 3:
+            talk2 = f'{ge[1]}:{ge[2]}分\n'
+
+        talk3 = f'攻击分为{atk}分,\n防御分为{rou}分,\n辅助分为{ob}分,\n总分为{score}分.\n'
+        talk = f'{talk1}{talk2}{talk3}一眼丁真:{talk4}'
+
+    await bot.send(ev, talk)
 
 
